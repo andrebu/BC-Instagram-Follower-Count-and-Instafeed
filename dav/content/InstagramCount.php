42ab0db7b4ac5cd0e4ca327d0cfa8bd31a253c0b
Andre Bulatov <andre.bulatov@gmail.com>
2016-04-09 19:04:34 -0400

    api improved

--- dav/content/InstagramCount.php
+++ dav/content/InstagramCount.php
@@ -1,6 +1,17 @@
 <?php
   header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');    
-  $instaAPI = file_get_contents("https://api.instagram.com/v1/users/1202155813/?access_token=1202155813.467ede5.8cc95a8e2bb54cec8c85a0c155f7e2e7");
-  echo json_encode($instaAPI);
+  $insta_count_obj = file_get_contents("https://api.instagram.com/v1/users/884494772/?access_token=1202155813.467ede5.8cc95a8e2bb54cec8c85a0c155f7e2e7");
+  $insta_media_obj = file_get_contents("https://api.instagram.com/v1/users/884494772/media/recent/?access_token=1202155813.467ede5.8cc95a8e2bb54cec8c85a0c155f7e2e7&scope=public_content");
+  $insta_meta_array = array("meta" => json_decode($insta_count_obj, true));
+  $insta_about_array = array("about" => json_decode($insta_media_obj, true));
+  $insta_api_data = json_encode(
+  	array_merge(
+  		$insta_meta_array,
+  		$insta_about_array
+  		)
+  	);
+
+  echo $insta_api_data;
+
 ?>
